f0e4bcde30cd6a1a64bedc8cd134830da9447501
Igor Leal <igorhenriqueleal3@gmail.com>
2022-10-08 15:29:40 -0300

    feat: Exercício sobre dataTypes concluído

---
+++ day00-dataTypes.js
@@ -0,0 +1,11 @@
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+  const firstInteger = 4;
+  const firstDecimal = 4.2;
+  const firstString = 'HackerRank ';
+
+  console.log(firstInteger + Number(secondInteger));
+  console.log(firstDecimal + Number(secondDecimal));
+  console.log(firstString + secondString);
+}
+performOperation(4, 4.0, 'is the best place to learn and practice coding!');
--- day00-helloWorld.js
+++ day00-helloWorld.js
@@ -1,4 +1,6 @@
-function greeting() {
+function greeting(parameterVariable) {
   console.log('Hello, World!');
+  console.log(parameterVariable);
 }
-greeting();
+
+greeting('Welcome to 10 Days of JavaScript!');
