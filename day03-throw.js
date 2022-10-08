eeb1e40ee48df611d9fd305d095df5b551ab8241
Igor Leal <igorhenriqueleal3@gmail.com>
2022-10-08 18:41:13 -0300

    feat: Exercício sobre throw concluído

---
+++ day03-throw.js
@@ -0,0 +1,13 @@
+function isPositive(a) {    
+  let result = 'YES';
+
+  if (a < 0) {
+      throw { message: 'Negative Error' };
+  } else if (a === 0) {
+      throw { message: 'Zero Error' };
+  }
+
+  return result;
+}
+
+console.log(isPositive(1));
